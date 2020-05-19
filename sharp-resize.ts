19891b0af2bf55a41cca27b0faefb3599abe3184
Felix Haus <felixhaus@gmail.com>
2020-05-19 20:56:24 +0200

    Adds typescript example

---
+++ sharp-resize.ts
@@ -0,0 +1,11 @@
+import sharp from 'sharp';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function main() {
+  const sourceImage = fs.readFileSync(path.join(__dirname, 'source.png'));
+  const resizedImage = await sharp(sourceImage).resize(200).toBuffer();
+  fs.writeFileSync(path.join(__dirname, 'out.png'), resizedImage);
+}
+
+main();
